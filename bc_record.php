b56c635370599a7cdac3148ae2d43d27bdd5d121
Joao Polonia <joaovpsilva@hotmail.com>
2018-08-16 15:45:10 +0100

    Second Commit

--- bc_record.php
+++ bc_record.php
@@ -1,11 +1,11 @@
 <?php
-    include_once('config/init.php');
-    include_once('database/paciente.php');
+include_once('config/init.php');
+include_once('database/paciente.php');
     /*$patients = getAllPatients();  
     $pacientes = json_encode($patients);
     print_r($patients);*/
-    include ('templates/header.php');
-    include ('templates/nav.php');
-    include ('templates/bc_record.php');
-	include ('templates/footer.php');
+include('templates/header.php');
+include('templates/nav.php');
+include('templates/bc_record.html');
+include('templates/footer.php');
 ?>
