eab10fa10104d1fd5831361ffa978a1159574b62
Joao Polonia <joaovpsilva@hotmail.com>
2018-08-21 00:03:11 +0100

    Initial Commit to github

--- templates/login.php
+++ templates/login.php
@@ -1,19 +1,22 @@
-    <div id="login">
-        <h1><i class="fa fa-heartbeat" aria-hidden="true"  style="font-size:250%"></i> </h1>
-        <h1>BC Record</h1>
-        <? if ($_SESSION['message']) { ?>
-            <div class="message">
-                <?= $_SESSION['message']; unset($_SESSION['message']) ?>
-            </div>
-        <? } ?>
-        <form method="post" action="action_login.php">
-            <input type="text" name="username" placeholder="Patient-ID/Medic-ID" value="<?=$_SESSION[form_values][username]?>"/>
-            <input type="password" name="password" placeholder="Password"/>
-            <input type="submit" id="login-btn" name="login" value="Login"/>                
-        </form>
+<div id="login">
+    <h1><i class="fa fa-heartbeat" aria-hidden="true" style="font-size:250%"></i> </h1>
+    <h1>BC Record</h1>
+    <? if ($_SESSION['message']) { ?>
+    <div class="message">
+        <?= $_SESSION['message'];
+        unset($_SESSION['message']) ?>
     </div>
-    <footer>
-		&copy; 2017 &middot; BCRecord
-    </footer>
-  </body>
+    <?
+} ?>
+    <form method="post" action="action_login.php">
+        <input type="text" name="username" placeholder="Patient-ID/Medic-ID" value="<?= $_SESSION[form_values][username] ?>" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="submit" id="login-btn" name="login" value="Login" />
+    </form>
+</div>
+<footer>
+    &copy; 2018 &middot; BCRecord
+</footer>
+</body>
+
 </html>
--- templates/nav.php
+++ templates/nav.php
@@ -12,32 +12,38 @@
     </div>
     <nav>
         <ul>
-                <li class="active"><a href="home.php"><span><i class="fa fa-tachometer"></i></span><span>Dashboard</span></a></li>
+            <li class="active"><a href="home.php"><span><i class="fa fa-tachometer"></i></span><span>Dashboard</span></a></li>
             <?php if ($_SESSION['role'] == "patient") { ?>
-                <li><a href="my_profile.php"><span><i class="fa fa-user-md"></i></span><span>My Profile</span></a></li>
-            <?php }if ($_SESSION['role'] == "medic") { ?>
-                <li><a href="my_profile.php"><span><i class="fa fa-user-md"></i></span><span>My Profile</span></a></li>
-                <li><a href="list_patients.php"><span><i class="fa fa-users"></i></span><span>Manage Patients</span></a></li>
-                <li><a href="list_conditions.php"><span><i class="fa fa-medkit "></i></span><span>Manage Conditions</span></a></li>
-                <li><a href="bc_record.php"><span><i class="fa fa-heartbeat "></i></span><span>BC Record App</span></a></li>
-            <?php }if ($_SESSION['role'] == "admin") { ?>
-                <li><a href=""><span><i class="fa fa-users"></i></span><span>Manage Users</span></a>
+            <li><a href="my_profile.php"><span><i class="fa fa-user-md"></i></span><span>My Profile</span></a></li>
+            <?php 
+        }
+        if ($_SESSION['role'] == "medic") { ?>
+            <li><a href="my_profile.php"><span><i class="fa fa-user-md"></i></span><span>My Profile</span></a></li>
+            <li><a href="list_patients.php"><span><i class="fa fa-users"></i></span><span>Manage Patients</span></a></li>
+            <li><a href="list_conditions.php"><span><i class="fa fa-medkit "></i></span><span>Manage Conditions</span></a></li>
+            <li><a href="bc_record.php"><span><i class="fa fa-heartbeat "></i></span><span>BC Record App</span></a></li>
+            <?php 
+        }
+        if ($_SESSION['role'] == "admin") { ?>
+            <li><a href=""><span><i class="fa fa-users"></i></span><span>Manage Users</span></a>
                 <ul>
                     <li><a href=""><span><i class="fa fa-plus"></i></span><span>Add</span></a></li>
                     <li><a href=""><span><i class="fa fa-edit"></i></span><span>Edit</span></a></li>
                     <li><a href=""><span><i class="fa fa-remove"></i></span><span>Remove</span></a></li>
                 </ul>
-                </li>
-            <?php }else{?> 
-                <li><a href="chat.php"><span><i class="fa  fa-pencil-square-o "></i></span><span>Forum</span></a></li>
-                <li><a href="list_appointments.php"><span><i class="fa fa-calendar-check-o "></i></span><span>Appointments</span></a></li>
-            <?php } ?> 
-                <li><a href="action_logout.php"><span><i class="fa fa-sign-out "></i></span><span>Logout</span></a></li>
+            </li>
+            <?php 
+        } else { ?>
+            <li><a href="chat.php"><span><i class="fa  fa-pencil-square-o "></i></span><span>Forum</span></a></li>
+            <li><a href="list_appointments.php"><span><i class="fa fa-calendar-check-o "></i></span><span>Appointments</span></a></li>
+            <?php 
+        } ?>
+            <li><a href="action_logout.php"><span><i class="fa fa-sign-out "></i></span><span>Logout</span></a></li>
         </ul>
     </nav>
     <footer>
-        <div >
-		    &copy; 2017  
+        <div>
+            &copy; 2018
         </div>
         <div>
             BC Records
